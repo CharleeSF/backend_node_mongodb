0dab0ef3617faeedebe8c8e641927c9919d2e10e
CharleeF <charleetje@gmail.com>
2018-01-05 16:12:15 +0100

    Using mlab for database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    //database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://charlee:hallo@ds239097.mlab.com:39097/meanauthapp',
     secret: 'yoursecret'
 }
